487b8fc8de95f2fefb8f68645dfb6b918a1fc585
Malva Malmgren <malva.malmgren@gmail.com>
2025-10-15 14:08:37 +0200

    Cleanup index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
-//import NoteView from "@/views/NoteView.vue";
-//import QuizView from "@/views/QuizView.vue";
 
-const routes = [
-  { path: "/", redirect: "/notes" },
-  //{ path: "/notes", component: NoteView },
-  //{ path: "/quiz", component: QuizView },
-];
+const routes = [{ path: "/", redirect: "/notes" }];
 
 const router = createRouter({
   history: createWebHistory(),
